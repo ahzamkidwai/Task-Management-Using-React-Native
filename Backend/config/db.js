94035daf74b9215d46d082100e41f6bad532d709
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-03-10 11:39:53 +0530

    Add DB Configuration

---
+++ Backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose
+      .connect(process.env.MONGODB_URI, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      })
+      .then(() => console.log("MongoDB connected successfully"))
+      .catch((err) => console.error("MongoDB connection error:", err));
+    console.log("MongoDB Connected!");
+  } catch (error) {
+    console.error("MongoDB Connection Failed!", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const app = express();
+const cors = require("cors");
 
 const PORT = 3000;
 
+// Load environment variables
+dotenv.config();
+
+connectDB();
+
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(cors());
 
 // Simple Route
 app.get("/", (req, res) => {
