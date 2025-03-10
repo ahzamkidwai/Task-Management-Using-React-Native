29d52330e7604392e993f83d27d106999b46b24e
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-03-10 12:52:50 +0530

    Add Login Route to Login user

--- Backend/controllers/auth-controller.js
+++ Backend/controllers/auth-controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userRegistration = async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
@@ -26,4 +28,47 @@ const userRegistration = async (req, res) => {
   }
 };
 
-module.exports = { userRegistration };
+const userLogginIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Email ID" });
+    }
+
+    // Compare provided password with hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET, // Ensure you have JWT_SECRET in your environment variables
+      { expiresIn: "12h" } // Token expires in 1 hour
+    );
+
+    // Send response with token
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name, // Include other user details if necessary
+      },
+    });
+    console.log("User Login Successful!!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occurred while loggin In" });
+  }
+};
+
+module.exports = { userRegistration, userLogginIn };
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { userRegistration } = require("../controllers/auth-controller");
+const {
+  userRegistration,
+  userLogginIn,
+} = require("../controllers/auth-controller");
 const router = express.Router();
 
 router.post("/register", userRegistration);
+router.post("/signin", userLogginIn);
 
 module.exports = router;
