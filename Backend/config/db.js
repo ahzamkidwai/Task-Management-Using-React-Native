77bbe04080f76ee4d86ce7439420a8771db3e8d9
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2025-03-15 11:42:47 +0530

    Change

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,32 +1,18 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     mongoose
-//       .connect(process.env.MONGODB_URI, {
-//         // useNewUrlParser: true,
-//         // useUnifiedTopology: true,
-//       })
-//       .then(() => console.log("MongoDB connected successfully"))
-//       .catch((err) => console.error("MongoDB connection error:", err));
-//     console.log("MongoDB Connected!");
-//   } catch (error) {
-//     console.error("MongoDB Connection Failed!", error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("✅ MongoDB connected successfully");
+    mongoose
+      .connect(process.env.MONGODB_URI, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      })
+      .then(() => console.log("MongoDB connected successfully"))
+      .catch((err) => console.error("MongoDB connection error:", err));
+    console.log("MongoDB Connected!");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed!", error);
-    process.exit(1); // Exit process with failure
+    console.error("MongoDB Connection Failed!", error);
+    process.exit(1);
   }
 };
 
